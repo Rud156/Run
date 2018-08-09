5fe40287a648f39fedb1d8e1a853f6d6d0292094
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-09 20:09:03 +0530

    Adding Basic BoilerPlate

--- Assets/Scripts/Static Content/TagManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TagManager
-{
-    public const string Player = "Player";
-    public const string Enemy = "Enemy";
-}
