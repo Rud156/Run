5b3f7c49209f3792779a66f40b9880de1d1cc698
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-10 00:09:07 +0530

    Added Basic Player And Camera Controller

---
+++ Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [Header("Base Stats")]
+    public float followSpeed = 10f;
+    public float lookSpeed = 10f;
+    public Vector3 cameraOffset;
+
+    [Header("Player")]
+    public Transform player;
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+
+    private void LookAtTarget()
+    {
+        Vector3 lookDirection = player.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, lookSpeed * Time.deltaTime);
+    }
+
+    private void MoveToTarget()
+    {
+        Vector3 targetPosition = player.position +
+            player.forward * cameraOffset.z +
+            player.right * cameraOffset.x +
+            player.up * cameraOffset.y;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/Movement/CarController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [Header("Wheel Colliders")]
+    public WheelCollider frontLWheelCollider;
+    public WheelCollider frontRWheelCollider;
+    public WheelCollider rearLWheelCollider;
+    public WheelCollider rearRWheelCollider;
+
+    [Header("Actual Wheel Objects")]
+    public Transform frontLWheel;
+    public Transform frontRWheel;
+    public Transform rearLWheel;
+    public Transform rearRWheel;
+
+    public float maxSteerAngle = 30;
+    public float motorForce = 50;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steerAngle;
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        GetInput();
+
+        Steer();
+        Accelerate();
+
+        UpdateWheelPoses();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        steerAngle = maxSteerAngle * horizontalInput;
+        frontLWheelCollider.steerAngle = steerAngle;
+        frontRWheelCollider.steerAngle = steerAngle;
+    }
+
+    private void Accelerate()
+    {
+        frontLWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRWheelCollider.motorTorque = verticalInput * motorForce;
+
+        rearLWheelCollider.motorTorque = verticalInput * motorForce;
+        rearRWheelCollider.motorTorque = verticalInput * motorForce;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontLWheelCollider, frontLWheel);
+        UpdateWheelPose(frontRWheelCollider, frontRWheel);
+        UpdateWheelPose(rearLWheelCollider, rearLWheel);
+        UpdateWheelPose(rearRWheelCollider, rearRWheel);
+    }
+
+    private void UpdateWheelPose(WheelCollider currentCollider, Transform currentTransform)
+    {
+        Vector3 position = currentTransform.position;
+        Quaternion rotation = currentTransform.rotation;
+
+        currentCollider.GetWorldPose(out position, out rotation);
+
+        currentTransform.position = position;
+        currentTransform.rotation = rotation;
+    }
+}
