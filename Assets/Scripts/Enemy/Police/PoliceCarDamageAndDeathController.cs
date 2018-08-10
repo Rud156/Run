791a560eb1abd66ed4757c509982017b0d2ff61c
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-11 00:27:04 +0530

    Updated Fire Prefab, Updated Enemy Health

--- Assets/Scripts/Enemy/Police/PoliceCarDamageAndDeathController.cs
+++ Assets/Scripts/Enemy/Police/PoliceCarDamageAndDeathController.cs
@@ -12,20 +12,25 @@ public class PoliceCarDamageAndDeathController : MonoBehaviour
     public float maxHealthLostFromCollision = 10;
     public float maxDamagePossible;
 
-    [Header("Police Car Health")]
-    public Color minHealthColor = Color.red;
-    public Color halfHealthColor = Color.yellow;
-    public Color maxHealthColor = Color.green;
-    public Slider policeCarHealthSlider;
-    public Image policeCarHealthFiller;
+    [Header("Vehicle Fire")]
+    public float healthRatioToSpawnFire = 0.75f;
+    public int minFireParticles = 30;
+    public int maxFireParticles = 100;
+    public GameObject vehicleFireSpawnPoint;
+    public GameObject vehicleFire;
 
     private float currentPoliceCarHealth;
+    private List<ParticleSystem> vehicleFireParticleSystem;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
     /// </summary>
-    void Start() => currentPoliceCarHealth = maxPoliceCarHealth;
+    void Start()
+    {
+        currentPoliceCarHealth = maxPoliceCarHealth;
+        vehicleFireParticleSystem = new List<ParticleSystem>();
+    }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
@@ -51,15 +56,32 @@ public class PoliceCarDamageAndDeathController : MonoBehaviour
 
     private void UpdateHealth()
     {
-        float maxHealth = maxPoliceCarHealth;
-        float currentHealthLeft = currentPoliceCarHealth;
-        float healthRatio = currentHealthLeft / maxHealth;
+        float currentHealthRatio = currentPoliceCarHealth / maxPoliceCarHealth;
+        if (currentHealthRatio >= healthRatioToSpawnFire)
+            return;
 
-        if (healthRatio <= 0.5)
-            policeCarHealthFiller.color = Color.Lerp(minHealthColor, halfHealthColor, healthRatio * 2);
-        else
-            policeCarHealthFiller.color = Color.Lerp(halfHealthColor, maxHealthColor, (healthRatio - 0.5f) * 2);
-        policeCarHealthSlider.value = healthRatio;
+        if (vehicleFireParticleSystem.Count == 0)
+        {
+            GameObject vehicleFireInstance = Instantiate(vehicleFire,
+                vehicleFireSpawnPoint.transform.position,
+                vehicleFire.transform.rotation);
+            vehicleFireInstance.transform.SetParent(gameObject.transform);
+
+            vehicleFireParticleSystem.Add(vehicleFireInstance.
+                transform.GetChild(0).GetComponent<ParticleSystem>());
+            vehicleFireParticleSystem.Add(vehicleFireInstance.
+                transform.GetChild(1).GetComponent<ParticleSystem>());
+        }
+
+        foreach (ParticleSystem item in vehicleFireParticleSystem)
+        {
+            float mappedEmission = ExtensionFunctions.Map(currentHealthRatio,
+                healthRatioToSpawnFire, 0,
+                minFireParticles, maxFireParticles);
+
+            ParticleSystem.EmissionModule emission = item.emission;
+            emission.rateOverTime = mappedEmission;
+        }
     }
 
     private void CheckHealthZero()
