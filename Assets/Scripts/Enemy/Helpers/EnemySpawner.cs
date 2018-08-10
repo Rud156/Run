9609c78ba04fc027cc302ca63c5e22bf3d8bc0c8
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-10 21:15:46 +0530

    Added Enemy Spawner

--- Assets/Scripts/Enemy/Helpers/EnemySpawner.cs
+++ Assets/Scripts/Enemy/Helpers/EnemySpawner.cs
@@ -2,15 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour {
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Spawn GameObjects")]
+    public List<Transform> spawnPoints;
+    public GameObject spawnEffect;
+    public GameObject enemy;
+    public GameObject spawnerHolder;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Spawn Stats")]
+    public float waitBetweenSpawn = 5f;
+    public float waitBeteweenEffectAndSpawn = 0.1f;
+
+    [Header("Debug")]
+    public bool spawnOnStart;
+
+    private Coroutine coroutine;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        if (spawnOnStart)
+            StartSpawn();
+    }
+
+    public void StartSpawn() =>
+        coroutine = StartCoroutine(SpawnEnemies());
+
+    public void StopSpawn() =>
+        StopCoroutine(coroutine);
+
+    IEnumerator SpawnEnemies()
+    {
+        int randomNumber = Random.Range(0, 1000);
+        int randomIndex = randomNumber % spawnPoints.Count;
+
+        Vector3 spawnPosition = spawnPoints[randomIndex].position;
+        Instantiate(spawnEffect, spawnPosition, spawnEffect.transform.rotation);
+
+        yield return new WaitForSeconds(waitBeteweenEffectAndSpawn);
+
+        GameObject enemyInstance = Instantiate(enemy, spawnPosition, enemy.transform.rotation);
+        enemyInstance.transform.SetParent(spawnerHolder.transform);
+
+        yield return new WaitForSeconds(waitBetweenSpawn);
+    }
 }
