9a34a80a224af26f87a67e7aca54d04021204a80
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-14 01:21:56 +0530

    Added Objective Arrow

---
+++ Assets/Scripts/Helpers/ObjectFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollowPlayer : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 positionOffset;
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = playerTransform.position +
+            playerTransform.forward * positionOffset.z +
+            playerTransform.right * positionOffset.x +
+            playerTransform.up * positionOffset.y;
+
+        transform.position = targetPosition;
+    }
+}
--- Assets/Scripts/Player/Mission/PlayerMissionSpawner.cs
+++ Assets/Scripts/Player/Mission/PlayerMissionSpawner.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerMissionSpawner : MonoBehaviour
 {
+    [Header("Mission")]
+    public GameObject missionStop;
+    public GameObject missionExplosion;
     public List<GameObject> missionSpawnPoints;
+
+    [Header("Player Helper")]
+    public Transform playerTransform;
+    public Transform arrowTransform;
+
+    [Header("Debug")]
     public bool debugOnStart;
 
     private GameObject missionBeginObject;
     private GameObject missionEndObject;
     private bool missionBeginValidated;
     private bool missionEndValidated;
+
+    private GameObject missionStopInstance;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -22,9 +33,32 @@ public class PlayerMissionSpawner : MonoBehaviour
             StartMissions();
     }
 
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate() => SelectObjectiveToPointAndSetDirection();
+
     public void StartMissions() =>
         SpawnMission();
 
+    private void SelectObjectiveToPointAndSetDirection()
+    {
+        if (missionBeginObject == null)
+            return;
+
+        if (!missionBeginValidated)
+            SetArrowDirection(missionBeginObject);
+        else
+            SetArrowDirection(missionEndObject);
+    }
+
+    private void SetArrowDirection(GameObject objective)
+    {
+        Vector3 directionToObjective = objective.transform.position -
+                playerTransform.position;
+        Quaternion rotation = Quaternion.LookRotation(directionToObjective);
+        arrowTransform.transform.rotation = rotation;
+    }
 
     private void SpawnMission()
     {
@@ -43,8 +77,12 @@ public class PlayerMissionSpawner : MonoBehaviour
         missionBeginObject = missionSpawnPoints[randomStartIndex];
         missionEndObject = missionSpawnPoints[randomEndIndex];
 
-        missionBeginObject.GetComponent<Renderer>().material.color = Color.red;
-        missionEndObject.GetComponent<Renderer>().material.color = Color.green;
+        if (missionStopInstance == null)
+            missionStopInstance = Instantiate(missionStop,
+               missionBeginObject.transform.position,
+               missionStop.transform.rotation);
+        else
+            missionStopInstance.transform.position = missionBeginObject.transform.position;
 
         missionBeginValidated = false;
         missionEndValidated = false;
@@ -57,14 +95,15 @@ public class PlayerMissionSpawner : MonoBehaviour
             if (!missionEndValidated && other == missionEndObject)
             {
                 missionEndValidated = true;
-                missionEndObject.GetComponent<Renderer>().material.color = Color.white;
+                Instantiate(missionExplosion, missionEndObject.transform.position, Quaternion.identity);
                 SpawnMission();
             }
         }
         else if (other == missionBeginObject)
         {
             missionBeginValidated = true;
-            missionBeginObject.GetComponent<Renderer>().material.color = Color.white;
+            Instantiate(missionExplosion, missionBeginObject.transform.position, Quaternion.identity);
+            missionStopInstance.transform.position = missionEndObject.transform.position;
         }
     }
 }
