7439175889a804838ae53c5d30e12fd7c01ddf48
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-14 18:24:32 +0530

    Updated Explosion Prefab

--- Assets/Scripts/Helpers/ObjectFollowPlayer.cs
+++ Assets/Scripts/Helpers/ObjectFollowPlayer.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class ObjectFollowPlayer : MonoBehaviour
 {
     public Transform playerTransform;
+    public bool keepPlayerRotation;
     public Vector3 positionOffset;
-
     /// <summary>
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     /// </summary>
     void FixedUpdate()
     {
+        Vector3 upVector = keepPlayerRotation ? playerTransform.up : Vector3.up;
+
         Vector3 targetPosition = playerTransform.position +
             playerTransform.forward * positionOffset.z +
             playerTransform.right * positionOffset.x +
-            playerTransform.up * positionOffset.y;
+            upVector * positionOffset.y;
 
         transform.position = targetPosition;
     }
