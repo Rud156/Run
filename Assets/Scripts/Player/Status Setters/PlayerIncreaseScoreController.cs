bc437d16de35566fd952ee7adf3b097fc8ca6856
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-15 12:10:53 +0530

    Adding Ramps

---
+++ Assets/Scripts/Player/Status Setters/PlayerIncreaseScoreController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerIncreaseScoreController : MonoBehaviour
+{
+    [Header("Score Stats")]
+    public GameObject scoreEffect;
+    public int minScoreAmount;
+    public int maxScoreAmount;
+    public float scoreIncreaseRate = 0.1f;
+
+    [Header("Reward Text")]
+    public TextMesh rewardTextMesh;
+    public Animator rewardTextAnimator;
+
+    [Header("UI Text")]
+    public Text uiScoreText;
+
+    private float currentScore;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start() => currentScore = 0;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        currentScore += scoreIncreaseRate * Time.deltaTime;
+        uiScoreText.text = $"Points: {currentScore}";
+    }
+
+    public void GenerateRandomScore()
+    {
+        int randomScore = Random.Range(minScoreAmount, maxScoreAmount);
+        rewardTextMesh.text = $"+{randomScore} Points";
+        rewardTextAnimator.SetTrigger(AnimatorVariables.DisplayText);
+
+        currentScore += randomScore;
+    }
+}
---
+++ Assets/Scripts/Player/Status Setters/PlayerIncreaseScoreTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIncreaseScoreTrigger : MonoBehaviour
+{
+    public PlayerIncreaseScoreController scoreController;
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        Rigidbody playerRB = other.GetComponent<Rigidbody>();
+        if (!playerRB || !other.CompareTag(TagManager.Player))
+            return;
+
+        scoreController.GenerateRandomScore();
+    }
+}
