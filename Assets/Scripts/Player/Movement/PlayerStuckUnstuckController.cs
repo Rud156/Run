dc71afa79b416b77f30b296137f6f16dbb66c7fa
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-19 00:35:23 +0530

    Updated Stuck Unstuck Trigger

---
+++ Assets/Scripts/Player/Movement/PlayerStuckUnstuckController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerStuckUnstuckController : MonoBehaviour
+{
+    public float waitTime = 5f;
+
+    private bool playerIsInBox;
+    private float startTime;
+    private Vector3 safePoint;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start() => playerIsInBox = false;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (!playerIsInBox)
+            return;
+
+        if (Time.time > startTime + waitTime)
+            ResetVehiclePosition();
+    }
+
+    public void ResetVehiclePosition()
+    {
+        transform.position = safePoint;
+        transform.rotation = Quaternion.identity;
+        SetPlayerExitedBox();
+    }
+
+    public void SetPlayerInBox(Vector3 safePoint)
+    {
+        playerIsInBox = true;
+        startTime = Time.time;
+        this.safePoint = safePoint;
+    }
+
+    public void SetPlayerExitedBox() => playerIsInBox = false;
+}
---
+++ Assets/Scripts/Player/Movement/PlayerStuckUnstuckTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStuckUnstuckTrigger : MonoBehaviour
+{
+    public PlayerStuckUnstuckController stuckUnstuckController;
+    public Transform safePoint;
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        Rigidbody target = other.GetComponent<Rigidbody>();
+        if (!target || !other.CompareTag(TagManager.Player))
+            return;
+
+        stuckUnstuckController.SetPlayerInBox(safePoint.position);
+    }
+
+    /// <summary>
+    /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerExit(Collider other)
+    {
+        Rigidbody target = other.GetComponent<Rigidbody>();
+        if (!target || !other.CompareTag(TagManager.Player))
+            return;
+
+        stuckUnstuckController.SetPlayerExitedBox();
+    }
+}
